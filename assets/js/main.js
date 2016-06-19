be70895e06920fb0bd0fcc50fcefbb427c58d59d
anirudt <anirudt@gmail.com>
2016-06-19 16:53:53 +0530

    added projs, pet projs, and blog feed

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,7 @@ jQuery(document).ready(function($) {
     $("#rss-feeds").rss(
     
         //Change this to your own rss feeds
-        "http://feeds.feedburner.com/TechCrunch/startups",
+        "https://feelote.wordpress.com/feed/",
         
         {
         // how many entries do you want?
