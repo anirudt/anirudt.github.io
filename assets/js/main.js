44c83d531753d7322c0cd74e6cb7ec3d127da644
anirudt <anirudt@gmail.com>
2016-06-19 17:01:54 +0530

    fixed ssl in rss feed plugin

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,6 +40,7 @@ jQuery(document).ready(function($) {
         // default: 'show'
         // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
         effect: 'slideFastSynced',
+        ssl: true,
         
         // outer template for the html transformation
         // default: "<ul>{entries}</ul>"
